ece17e1ad013175329b5dd4f28fc09641a70d14e
Zelle <danielowsarie5@gmail.com>
2026-01-20 08:59:08 -1200

    refactored app.py

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,7 +1,15 @@
 import os
 from app import create_app
+from flask_cors import CORS
 
-app = create_app(os.getenv('FLASK_ENV', 'development'))
+
+env = os.getenv('FLASK_ENV', 'development')
+app = create_app(env)
+
+
+allowed_origins = os.getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:4200').split(',')
+
+CORS(app, resources={r"/*": {"origins": allowed_origins}}, supports_credentials=True)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
